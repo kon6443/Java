36fa9248c8935a116f798c1eab6624a516b0529c
kon6443@gmail.com <kon6443@gmail.com>
2022-04-30 15:36:02 +0900

    interface practice

--- First/src/Main.java
+++ First/src/Main.java
@@ -1,23 +1,12 @@
 import java.util.Arrays;
 
-class Spell {
-    private String name;
-    protected void setName(String name) {
-        this.name = name;
-    }
-    public void ignite() {
-        System.out.println(this.name + " has used ignite.");
-    }
-    public void flash() {
-        System.out.println(this.name + " has used flash.");
-    }
-}
-class Champion extends Spell{   // Champion class inherited Spell class.
-    private String name;
-    private String q;
-    private String w;
-    private String e;
-    private String r;
+class Champion {
+    // protected variables can be accessed in the same packages or inherited classes.
+    protected String name;
+    protected String q;
+    protected String w;
+    protected String e;
+    protected String r;
 
     // Constructor.
     Champion(String name, String q, String w, String e, String r) {
@@ -26,16 +15,60 @@ class Champion extends Spell{   // Champion class inherited Spell class.
         this.w = w;
         this.e = e;
         this.r = r;
-        setName(name);  // this method came from Spell class.
+    }
+    public void ignite() {
+        System.out.println(this.name + " has used ignite.");
+    }
+    public void flash() {
+        System.out.println(this.name + " has used flash.");
+    }
+}
+
+class AD extends Champion implements Champ {   // AD class inherited Champion class, Champ interface can ues declared methods.
+    // Constructor.
+    AD(String name, String q, String w, String e, String r) {
+        // super() method calls Champion constructor in Champion class.
+        super(name, q, w, e, r);
     }
 
-    // method.
+    // methods.
+    public void primaryDMG() {
+        System.out.println(this.name + " primarily uses attack damage(AD).");
+    }
     public void usesCombo() {
         System.out.println(this.name + " using combo:: ");
         System.out.println("q: "+ this.q + ", w: "+ this.w + ", e: "+ this.e + ", r: "+ this.r);
     }
 }
 
+class AP extends Champion implements Champ {   // AP class inherited Champion class, Champ interface can ues declared methods.
+    // Constructor.
+    AP(String name, String q, String w, String e, String r) {
+        // super() method calls Champion constructor in Champion class.
+        super(name, q, w, e, r);
+    }
+    // methods.
+    public void primaryDMG() {
+        System.out.println(this.name + " primarily uses ability point(AP).");
+    }
+    public void usesCombo() {
+        System.out.println(this.name + " using combo:: ");
+        System.out.println("q: "+ this.q + ", w: "+ this.w + ", e: "+ this.e + ", r: "+ this.r);
+    }
+}
+
+// Displaying which champ primarily uses either AD or AP.
+class PrimaryDamage {
+    public void attack(Champ champ) {
+        champ.primaryDMG();
+    }
+}
+
+// Interface.
+interface Champ {
+    // This method is defined in AD or AP class.
+    void primaryDMG();
+}
 public class Main {
     // static keyword makes available to use method without generating an object.
     public static void main(String[] args) {
@@ -45,11 +78,22 @@ public class Main {
         System.out.println(Arrays.toString(num));
         System.out.println(word); // char array does not need to use Arrays.toString() method to print out itself.
         System.out.print(word.length + "\n");
+        System.out.println();
 
-        Champion talon = new Champion("talon", "Noxian Diplomacy", "Rake", "Assasin's path", "Ult");
+        AD talon = new AD("Talon", "Noxian Diplomacy", "Rake", "Assasin's path", "Ult");
+        AP leblanc = new AP("LeBlanc", "Sigil of Malice", "Distortion", "Ethereal Chains", "Ult");
+        PrimaryDamage dmgCheck = new PrimaryDamage(); // Displaying which champ primarily uses either AD or AP.
+
         talon.usesCombo();
-        talon.ignite(); // Champion object is using inherited method that came from Spell class.
-        talon.flash(); // Champion object is using inherited method that came from Spell class.
+        talon.ignite(); // AD object is using inherited method that came from Spell class.
+        talon.flash(); // AD object is using inherited method that came from Spell class.
+        dmgCheck.attack(talon); // AD
 
+        System.out.println();
+
+        leblanc.usesCombo();
+        leblanc.ignite(); // AP object is using inherited method that came from Spell class.
+        leblanc.flash(); // AP object is using inherited method that came from Spell class.
+        dmgCheck.attack(leblanc); // AP
     }
 }
