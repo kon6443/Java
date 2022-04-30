d31d87da8b6c68b3f67e487ef3ff9955cd4c80e7
kon6443@gmail.com <kon6443@gmail.com>
2022-04-30 12:13:39 +0900

    java calss, inheritance

--- First/src/Main.java
+++ First/src/Main.java
@@ -8,44 +8,36 @@ class Spell {
     public void ignite() {
         System.out.println(this.name + " has used ignite.");
     }
+    public void flash() {
+        System.out.println(this.name + " has used flash.");
+    }
 }
-class Champion extends Spell{
+class Champion extends Spell{   // Champion class inherited Spell class.
     private String name;
     private String q;
     private String w;
     private String e;
     private String r;
+
+    // Constructor.
     Champion(String name, String q, String w, String e, String r) {
         this.name = name;
         this.q = q;
         this.w = w;
         this.e = e;
         this.r = r;
-        setName(name);
+        setName(name);  // this method came from Spell class.
     }
+
+    // method.
     public void usesCombo() {
         System.out.println(this.name + " using combo:: ");
         System.out.println("q: "+ this.q + ", w: "+ this.w + ", e: "+ this.e + ", r: "+ this.r);
     }
 }
 
-class Animal {
-    public void sleep() {
-        System.out.println("Sleeping...");
-    }
-}
-
-class Dog extends Animal {
-    private String name;
-    Dog(String name) {
-        this.name = name;
-    }
-    public void sleep() {
-        System.out.println(this.name + " is sleeping...");
-    }
-}
-
 public class Main {
+    // static keyword makes available to use method without generating an object.
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int [] num = {1,2,3,4,5};
@@ -56,9 +48,8 @@ public class Main {
 
         Champion talon = new Champion("talon", "Noxian Diplomacy", "Rake", "Assasin's path", "Ult");
         talon.usesCombo();
-        talon.ignite();
+        talon.ignite(); // Champion object is using inherited method that came from Spell class.
+        talon.flash(); // Champion object is using inherited method that came from Spell class.
 
-//        Animal dog = new Dog("dog");
-//        dog.sleep();
     }
 }
